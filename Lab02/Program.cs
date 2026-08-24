1b80e37af0b781617c2c762ff041ecc6a88b5224
Siraphat111 <siraphatdarasa@gmail.com>
2026-08-24 16:34:32 +0700

    addlap02 header

---
+++ Lab02/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Student ID :1690703549
+ * Name       :Siraphat Darasa
+ * Section    :129B
+ * No.        : NA
+ * Course     : GI113 Computer Programming (GI)
+ */
+
+namespace Lab02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
