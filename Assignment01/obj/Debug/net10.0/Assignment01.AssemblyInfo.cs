0e7ecb09e6d533dcc2e7582a7a04deeee3c0b683
Siraphat111 <siraphatdarasa@gmail.com>
2026-09-20 19:29:04 +0700

    Update Assignment01.AssemblyInfo.cs

--- Assignment01/obj/Debug/net10.0/Assignment01.AssemblyInfo.cs
+++ Assignment01/obj/Debug/net10.0/Assignment01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99aeba2ca7cb8a22f73bc83932ed29e5a315e8af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a6167099d896c22aed8c55896fe6180475cddbf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
