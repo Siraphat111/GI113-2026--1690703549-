e609e8ba6065fa09c7857045cea1f9312dd417cd
Siraphat111 <siraphatdarasa@gmail.com>
2026-09-21 16:22:47 +0700

    Lab06

---
+++ Lab06/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Lab06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int live = 5;
+
+            if (live == 0)
+            {
+                Console.WriteLine("Game Over");
+            }
+            else
+            {
+                Console.WriteLine("Continue to play!");
+            }
+
+
+            Console.WriteLine("Continue to run");
+
+            int level = 7;
+
+            if (level >= 10)
+            {
+                Console.WriteLine("The door opens.");
+            }
+            else if (level >= 5)
+            {
+                Console.WriteLine("Boss floor unlocked.");
+            }
+            else
+            {
+                Console.WriteLine("The door stays shut.");
+            }
+        }
+    }
+}
